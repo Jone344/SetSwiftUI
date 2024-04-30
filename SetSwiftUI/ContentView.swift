fa6ad127e53eff2e9cff8551adab49ff11eb8499
mando <76523408+spenser-jony@users.noreply.github.com>
2024-04-30 23:40:49 +0200

    Initial Commit

---
+++ SetSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetSwiftUI
+//
+//  Created by user on 30.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetSwiftUI/SetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetSwiftUIApp.swift
+//  SetSwiftUI
+//
+//  Created by user on 30.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
