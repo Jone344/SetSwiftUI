da5056fb60dfcfee362f03ee2239d23be63c8ce7
mando <76523408+spenser-jony@users.noreply.github.com>
2024-05-01 14:02:24 +0200

    viewmodel

--- SetSwiftUI/Set.swift
+++ SetSwiftUI/Set.swift
@@ -1,32 +1,8 @@
 //
-//  SetGame.swift
+//  Set.swift
 //  SetSwiftUI
 //
 //  Created by user on 01.05.24.
 //
 
 import Foundation
-
-import Foundation
-
-class Set {
-    
-    var deck = Deck()
-    var cardsOnTable = [Card]()
-    var selectedCards = [Card]()
-    var tryMatchCards = [Card]()
-    var alreadyMatched = [Card]()
-    
-    func getSelectedCards(index: Int) {
-        if selectedCards.count < 4 {
-            if !selectedCards.contains(cardsOnTable[index]) {
-                selectedCards.append(cardsOnTable[index])
-            } else {
-                if selectedCards.count < 3 {
-                    if let indexTouchedRepeat = selectedCards.firstIndex(of: cardsOnTable[index]) {
-                        selectedCards.remove(at: indexTouchedRepeat)
-                    }
-                }
-            }
-        }
-    }
